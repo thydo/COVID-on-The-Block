d8692411996c76ed5a6a6ed2b2b0ede5d2157581
achang-1 <57458670+achang-1@users.noreply.github.com>
2020-10-11 03:44:51 +0000

    Update csv2kml.py
    Inputted the values correctly

--- pyscripts/csv2kml.py
+++ pyscripts/csv2kml.py
@@ -1,9 +1,11 @@
-# kml = simple.Kml()
-# Location_name = "lolz"
-# coordinate_1 = "18.432314"
-# coordinate_2 = "-33.90862"
-# kml.newpoint(name=Location_name, coords=[(coordinate_1,coordinate_2)])  # lon, lat, optional height
-# kml.save("temp.kml")
+""" sample how-to make a KML 
+    kml = simple.Kml()
+    Location_name = "lolz"
+    coordinate_1 = "18.432314"
+    coordinate_2 = "-33.90862"
+    kml.newpoint(name=Location_name, coords=[(coordinate_1,coordinate_2)])  # lon, lat, optional height
+    kml.save("temp.kml")
+"""
 
 import csv
 import simplekml
@@ -12,21 +14,11 @@ kml = simplekml.Kml()
 
 with open ("communityresources - Form Responses.csv", newline = '', encoding='utf-8') as f: 
     csvinfo = csv.DictReader(f)
-
-    
     for rows in csvinfo: 
-
-        
-        if(rows['Do you have any resources for the community?']=="Yes"):
-            for push in rows:  
-                print(push)
-                resourceName = push['What type of resource(s) do you have?']
-                latitude = push['Where do you want to donate? (Latitude)']                 
-                longitude = push['Where do you want to donate? (Longitude)']
+        if(rows['What type of resource(s) do you have?']!=''):
+            resourceName = rows['What type of resource(s) do you have?']
+            latitude = rows['Where do you want to donate? (Latitude)']                 
+            longitude = rows['Where do you want to donate? (Longitude)']
             kml.newpoint(name = resourceName, coords=[(latitude,longitude)]) 
 
-kml.save('localhelp.kml')
-
-
-
-    
+kml.save('localresources.kml')    
